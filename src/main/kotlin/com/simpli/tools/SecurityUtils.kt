2896889bc80ecb13ae65a0e4167d3c30452d2d31
Gil Lopes Bueno <gil.bueno@martinlabs.com.br>
2017-10-19 23:45:29 -0200

    first commit

---
+++ src/main/kotlin/com/simpli/tools/SecurityUtils.kt
@@ -0,0 +1,171 @@
+package com.simpli.tools
+
+import java.io.UnsupportedEncodingException
+import java.math.BigInteger
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
+import java.util.Formatter
+import java.security.Key
+import java.security.spec.KeySpec
+import java.util.Base64
+
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
+import org.apache.commons.codec.digest.DigestUtils
+import java.nio.charset.Charset
+
+/**
+ *
+ * @author gil
+ */
+object SecurityUtils {
+
+    @JvmOverloads
+    fun encrypt(raw: String, key: String, encode: Boolean = false): String? {
+        try {
+            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+            val iv = byteArrayOf(1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 7, 7, 7, 7)
+            cipher.init(Cipher.ENCRYPT_MODE, generateKey(key), IvParameterSpec(iv))
+            val b = raw.toByteArray(charset("UTF-8"))
+            val ciphertext = cipher.doFinal(b, 0, b.size)
+            val encoded = Base64.getEncoder().encode(ciphertext)
+            var encryptedValue = String(encoded, charset("UTF-8"))
+
+            if (encode) {
+                encryptedValue = URLEncoder.encode(encryptedValue, "UTF-8")
+            }
+
+            return encryptedValue
+        } catch (ex: Exception) {
+            return null
+        }
+
+    }
+
+    @JvmOverloads
+    fun decrypt(encrypted: String, key: String, decode: Boolean = false): String? {
+        var encrypted = encrypted
+        try {
+            if (decode) {
+                encrypted = URLDecoder.decode(encrypted, "UTF-8")
+            }
+
+            val decoded = Base64.getDecoder().decode(encrypted.toByteArray())
+            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+            val iv = byteArrayOf(1, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 1, 7, 7, 7, 7)
+            cipher.init(Cipher.DECRYPT_MODE, generateKey(key), IvParameterSpec(iv))
+            val decryptedBytes = cipher.doFinal(decoded, 0, decoded.size)
+            return String(decryptedBytes, charset("UTF-8"))
+        } catch (ex: Exception) {
+            return null
+        }
+
+    }
+
+    fun encode(token: String, enc: String): String? {
+        try {
+            return URLEncoder.encode(token, enc)
+        } catch (ex: UnsupportedEncodingException) {
+            ex.printStackTrace()
+            return null
+        }
+
+    }
+
+    fun decode(token: String, enc: String): String? {
+        try {
+            return URLDecoder.decode(token, enc)
+        } catch (ex: UnsupportedEncodingException) {
+            ex.printStackTrace()
+            return null
+        }
+
+    }
+
+    fun generateRandomString(length: Int): String {
+        val random = SecureRandom()
+
+        return BigInteger(length * 5, random).toString(32)
+    }
+
+    fun randInt(min: Int, max: Int): Int {
+        val random = SecureRandom()
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        return random.nextInt(max - min + 1) + min
+    }
+
+    fun generateHash(subject: Any, prime: Long): String {
+        return sha1((subject.hashCode() * prime).toString() + "")
+    }
+
+    fun md5(input: String?): String? {
+
+        var md5: String? = null
+
+        if (null == input) return null
+
+        try {
+
+            //Create MessageDigest object for MD5
+            val digest = MessageDigest.getInstance("MD5")
+
+            //Update input string in message digest
+            digest.update(input.toByteArray(), 0, input.length)
+
+            //Converts message digest value in base 16 (hex)
+            md5 = BigInteger(1, digest.digest()).toString(16)
+
+        } catch (e: NoSuchAlgorithmException) {
+        }
+
+        return md5
+    }
+
+    fun sha1(subject: String): String {
+        var sha1 = ""
+        try {
+            val crypt = MessageDigest.getInstance("SHA-1")
+            crypt.reset()
+            crypt.update(subject.toByteArray(charset("UTF-8")))
+            sha1 = byteToHex(crypt.digest())
+        } catch (e: NoSuchAlgorithmException) {
+
+        } catch (e: UnsupportedEncodingException) {
+
+        }
+
+        return sha1
+    }
+
+    fun sha256(subject: String): String {
+        return DigestUtils.sha256Hex(subject)
+    }
+
+    private fun byteToHex(hash: ByteArray): String {
+        val formatter = Formatter()
+        for (b in hash) {
+            formatter.format("%02x", b)
+        }
+        val result = formatter.toString()
+        formatter.close()
+        return result
+    }
+
+    @Throws(Exception::class)
+    private fun generateKey(key: String): Key {
+        val salt = byteArrayOf(-84, -119, 25, 56, -100, 100, -120, -45, 84, 67, 96, 10, 24, 111, 112, -119, 3)
+        val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+        val spec = PBEKeySpec(key.toCharArray(), salt, 1024, 128)
+        val tmp = factory.generateSecret(spec)
+        return SecretKeySpec(tmp.encoded, "AES")
+    }
+
+}
---
+++ src/main/kotlin/com/simpli/tools/Validator.kt
@@ -0,0 +1,255 @@
+package com.simpli.tools
+
+import java.util.InputMismatchException
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+/**
+ *
+ * @author ricardoprado
+ */
+object Validator {
+
+    /**
+     * Formats using (xx) xxxxx-xxxx
+     * @param celNumber
+     * @return
+     */
+
+    fun formatCelPhone(celNumber: String): String {
+        var celNumber = celNumber
+        celNumber = "(" + celNumber.substring(0, 2) + ")" + celNumber.substring(2, celNumber.length)
+        celNumber = celNumber.substring(0, 4) + celNumber.substring(4, 9) + "-" + celNumber.substring(9, celNumber.length)
+        return celNumber
+    }
+
+    fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        val pattern: Pattern
+        val matcher: Matcher
+        val patStr = "^\\+?[1-9]\\d{10,14}$"
+
+        pattern = Pattern.compile(patStr)
+        matcher = pattern.matcher(phoneNumber)
+        return matcher.matches()
+    }
+
+    //Ex Celulares validos : 11996988588 , 011996988588 , 11196988588 , 5511996988588 , +5511996988588
+    //Ex Celulares invalidos : 11096988588 , 01996988588 , 11996899889899898989 , +0511996988588 , 0511996988588
+    fun isValidCelPhoneNumber(phone: String): Boolean {
+        val pattern: Pattern
+        val matcher: Matcher
+        val patStr = "^(\\+?[1-9]{2})?([1-9]{2}|0[1-9]{2})[1-9][0-9]{8}$"
+
+        pattern = Pattern.compile(patStr)
+        matcher = pattern.matcher(phone)
+        return matcher.matches()
+
+    }
+
+    fun isEmail(email: String?): Boolean {
+        if (email == null) {
+            return false
+        }
+
+        val pattern: Pattern
+        val matcher: Matcher
+        val patStr = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+
+        pattern = Pattern.compile(patStr)
+        matcher = pattern.matcher(email)
+
+        return matcher.matches()
+    }
+
+    fun isCorporativeEmail(email: String): Boolean {
+        if (email.indexOf("gmail.com") != -1) {
+            return false
+        } else if (email.indexOf("yahoo.com") != -1) {
+            return false
+        } else if (email.indexOf("yahoo.com.br") != -1) {
+            return false
+        } else if (email.indexOf("ymail.com") != -1) {
+            return false
+        } else if (email.indexOf("rocketmail.com") != -1) {
+            return false
+        } else if (email.indexOf("bol.com.br") != -1) {
+            return false
+        } else if (email.indexOf("hotmail.com") != -1) {
+            return false
+        } else if (email.indexOf("hotmail.com.br") != -1) {
+            return false
+        } else if (email.indexOf("live.com") != -1) {
+            return false
+        } else if (email.indexOf("msn.com") != -1) {
+            return false
+        } else if (email.indexOf("ig.com.br") != -1) {
+            return false
+        } else if (email.indexOf("globomail.com.br") != -1) {
+            return false
+        } else if (email.indexOf("oi.com.br") != -1) {
+            return false
+        } else if (email.indexOf("pop.com.br") != -1) {
+            return false
+        } else if (email.indexOf("inteligweb.com.br") != -1) {
+            return false
+        } else if (email.indexOf("folha.com.br") != -1) {
+            return false
+        } else if (email.indexOf("zipmail.com.br") != -1) {
+            return false
+        }
+        return true
+    }
+
+    fun isCPF(CPF: String?): Boolean {
+        var CPF: String = CPF ?: return false
+
+        CPF = CPF.replace("-", "")
+        CPF = CPF.replace(".", "")
+
+        // considera-se erro CPF's formados por uma sequencia de numeros iguais
+        if (CPF == "00000000000" || CPF == "11111111111" ||
+                CPF == "22222222222" || CPF == "33333333333" ||
+                CPF == "44444444444" || CPF == "55555555555" ||
+                CPF == "66666666666" || CPF == "77777777777" ||
+                CPF == "88888888888" || CPF == "99999999999" ||
+                CPF.length != 11)
+            return false
+
+        val dig10: Char
+        val dig11: Char
+        var sm: Int
+        var i: Int
+        var r: Int
+        var num: Int
+        var peso: Int
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+            // Calculo do 1o. Digito Verificador
+            sm = 0
+            peso = 10
+            i = 0
+            while (i < 9) {
+                // converte o i-esimo caractere do CPF em um numero:
+                // por exemplo, transforma o caractere '0' no inteiro 0
+                // (48 eh a posicao de '0' na tabela ASCII)
+                num = (CPF[i].toInt() - 48).toInt()
+                sm += num * peso
+                peso -= 1
+                i++
+            }
+
+            r = 11 - sm % 11
+            if (r == 10 || r == 11)
+                dig10 = '0'
+            else
+                dig10 = (r + 48).toChar() // converte no respectivo caractere numerico
+
+            // Calculo do 2o. Digito Verificador
+            sm = 0
+            peso = 11
+            i = 0
+            while (i < 10) {
+                num = (CPF[i].toInt() - 48).toInt()
+                sm += num * peso
+                peso -= 1
+                i++
+            }
+
+            r = 11 - sm % 11
+            if (r == 10 || r == 11)
+                dig11 = '0'
+            else
+                dig11 = (r + 48).toChar()
+
+            // Verifica se os digitos calculados conferem com os digitos informados.
+            return dig10 == CPF[9] && dig11 == CPF[10]
+        } catch (erro: InputMismatchException) {
+            return false
+        }
+
+    }
+
+    fun isCNPJ(CNPJ: String): Boolean {
+        var CNPJ = CNPJ
+        var saida = true
+        var formatCNPJ = CNPJ
+        formatCNPJ = CNPJ.replace(".", "")
+        formatCNPJ = formatCNPJ.replace("/", "")
+        formatCNPJ = formatCNPJ.replace("-", "")
+
+        CNPJ = formatCNPJ
+        if (CNPJ == "00000000000000"
+                || CNPJ == "11111111111111"
+                || CNPJ == "22222222222222"
+                || CNPJ == "33333333333333"
+                || CNPJ == "44444444444444"
+                || CNPJ == "55555555555555"
+                || CNPJ == "66666666666666"
+                || CNPJ == "77777777777777"
+                || CNPJ == "88888888888888"
+                || CNPJ == "99999999999999"
+                || CNPJ.length != 14) {
+            saida = false
+        } else {
+            val dig13: Char
+            val dig14: Char
+            var sm: Int
+            var i: Int
+            var r: Int
+            var num: Int
+            var peso: Int
+
+            try {
+                sm = 0
+                peso = 2
+                i = 11
+                while (i >= 0) {
+                    num = (CNPJ[i].toInt() - 48).toInt()
+                    sm += num * peso
+                    peso += 1
+                    if (peso == 10) {
+                        peso = 2
+                    }
+                    i--
+                }
+
+                r = sm % 11
+                if (r == 0 || r == 1) {
+                    dig13 = '0'
+                } else {
+                    dig13 = (11 - r + 48).toChar()
+                }
+
+                sm = 0
+                peso = 2
+                i = 12
+                while (i >= 0) {
+                    num = (CNPJ[i].toInt() - 48).toInt()
+                    sm += num * peso
+                    peso += 1
+                    if (peso == 10) {
+                        peso = 2
+                    }
+                    i--
+                }
+
+                r = sm % 11
+                if (r == 0 || r == 1) {
+                    dig14 = '0'
+                } else {
+                    dig14 = (11 - r + 48).toChar()
+                }
+
+                saida = dig13 == CNPJ[12] && dig14 == CNPJ[13]
+            } catch (ex: Exception) {
+                saida = false
+            }
+
+        }
+
+        return saida
+
+    }
+
+}
