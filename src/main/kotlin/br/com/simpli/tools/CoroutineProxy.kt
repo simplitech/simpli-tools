6d71482662e69321394467bb8801232350ffbad5
jplippi <joao.p.lippi@gmail.com>
2019-10-15 16:44:46 -0300

    Changed kotlin version to 1.3.50, changed coroutine from experimental to release, and added CoroutineProxy.runInParallel() for running multiple callables at once

--- src/main/kotlin/br/com/simpli/tools/CoroutineProxy.kt
+++ src/main/kotlin/br/com/simpli/tools/CoroutineProxy.kt
@@ -1,13 +1,26 @@
 package br.com.simpli.tools
 
-import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.*
 import java.lang.Runnable
+import java.util.concurrent.Callable
 
 class CoroutineProxy {
     companion object {
         @JvmStatic
         fun asyncIt(runnable: Runnable) {
             GlobalScope.launch { runnable.run() }
+        }
+
+        @JvmStatic
+        fun <T :Any> runInParallel(vararg callable: Callable<T>): List<T> {
+            return runBlocking<List<T>> {
+                callable.map { GlobalScope.async { it.call() } }.awaitAll()
+            }
+        }
+
+        @JvmStatic
+        fun <T :Any> runInParallel(callable: List<Callable<T>>): List<T> {
+            return this.runInParallel(*callable.toTypedArray())
         }
     }
 }
