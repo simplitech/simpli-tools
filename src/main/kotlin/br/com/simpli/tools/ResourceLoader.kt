a5a9c251dd9abeddd9b67189b7552be8f01821bf
Joao Lippi <joao.lippi@simpli.com.br>
2019-12-04 21:57:44 -0300

    Added ResourceLoader object for reading files and other streams

--- src/main/kotlin/br/com/simpli/tools/ResourceLoader.kt
+++ src/main/kotlin/br/com/simpli/tools/ResourceLoader.kt
@@ -1,4 +1,36 @@
 package br.com.simpli.tools
 
+import java.io.InputStream
+import java.util.*
+
 object ResourceLoader {
+
+    fun getProperties(path: String, vararg classes: Class<*>): Properties {
+        return Properties().apply {
+            getStreamResource(path, *classes)?.also {
+                load(it)
+            }?.also {
+                it.close()
+            }
+        }
+    }
+
+    fun getStreamResource(path: String, vararg classes: Class<*>): InputStream? {
+        return Thread.currentThread().contextClassLoader?.getResourceAsStream(path)
+            ?: classes.getFirstStream(path)
+            ?: this.javaClass.getResourceAsStream(path)
+    }
+
+    private fun Array<out Class<*>>.getFirstStream(path: String): InputStream? {
+
+        var stream: InputStream? = null
+        this.forEach {
+            it.getResourceAsStream(path)?.apply {
+                stream = this
+                return@forEach
+            }
+        }
+
+        return stream
+    }
 }
